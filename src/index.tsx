b57e4dddd03c5de49319b7cd0835adf077baa359
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-03-22 18:38:46 -0300

    feat: :lipstick: adiciona o normalize.css ao projeto

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
+import 'normalize.css';
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import Menu from "./pages/Menu";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <>
-    <div>Aluroni</div>
+    <Menu />
   </>
 );
