529bcb59a2001323de7458e14a5f93c19210ce96
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-03-23 09:54:07 -0300

    feat: :sparkles: cria o componente search

---
+++ src/pages/Menu/Search/index.tsx
@@ -0,0 +1,15 @@
+import { CgSearch } from 'react-icons/cg';
+import styles from './Search.module.scss';
+interface Props {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Search({search, setSearch}: Props) {
+  return (
+    <div className={styles.buscador}>
+      <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+      <CgSearch size={20} color="#4C4D5E"/>
+    </div>
+  )
+}
